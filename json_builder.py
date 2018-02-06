18c7884c75c52785c91701673b3fa2ade1151008
Rouge <code@r0uge.org>
2018-02-05 19:07:10 -0800

    Add hash to JSON and a text file

--- json_builder.py
+++ json_builder.py
@@ -1,12 +1,16 @@
 import json
 import csv
 import urllib.request
+import hashlib
 
-DICT = {}
 URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeYoWsTDrgykb-_xR6Z0U3IDbo8c485s1FpQdyQcWp63TfsrtczOmfPs2d0s6bzVSAYZ4HMJcrNez3/pub?gid=0&single=true&output=csv'
 REQ = urllib.request.Request(URL)
 RES = urllib.request.urlopen(REQ)
-RES_DATA = RES.read().decode('ascii').replace('\r','')
+RES_DATA = RES.read().decode('utf-8').replace('\r','')
+HASH = hashlib.sha256(RES_DATA.encode('utf-8')).hexdigest()
+DICT = {'hash': HASH[:20]}
+with open('data/latest_hash.txt', 'w') as h:
+    h.write(HASH)
 with open('data/keyword.csv', 'w') as f:
     f.write(RES_DATA)
 with open('data/keyword.csv', 'r') as g:
@@ -21,5 +25,5 @@ with open('data/keyword.csv', 'r') as g:
         if len(cats) == 0:
             cats = 'null'
         DICT[key] = {'categories': cats, 'comments': row['comment'] if not row['comment'] == '' else 'null', 'used': False}
-with open('keywords.json', 'w') as o:
+with open('data/keywords.json', 'w') as o:
     json.dump(DICT, o)
