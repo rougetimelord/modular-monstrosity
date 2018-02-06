c2d45f4fa0dd3053d30131eea99f71ab0d031a55
Rouge <code@r0uge.org>
2018-02-05 18:55:03 -0800

    Automatically download csv from google drive

--- json_builder.py
+++ json_builder.py
@@ -1,9 +1,16 @@
 import json
 import csv
+import urllib.request
 
-dict = {}
-with open('data/keyword.csv') as f:
-    reader = csv.DictReader(f)
+DICT = {}
+URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeYoWsTDrgykb-_xR6Z0U3IDbo8c485s1FpQdyQcWp63TfsrtczOmfPs2d0s6bzVSAYZ4HMJcrNez3/pub?gid=0&single=true&output=csv'
+REQ = urllib.request.Request(URL)
+RES = urllib.request.urlopen(REQ)
+RES_DATA = RES.read().decode('ascii').replace('\r','')
+with open('data/keyword.csv', 'w') as f:
+    f.write(RES_DATA)
+with open('data/keyword.csv', 'r') as g:
+    reader = csv.DictReader(g)
     for row in reader:
         key = row['keyword']
         cats = []
@@ -13,6 +20,6 @@ with open('data/keyword.csv') as f:
                 cats.append(row[cat_key])
         if len(cats) == 0:
             cats = 'null'
-        dict[key] = {'categories': cats, 'comments': row['comment'] if not row['comment'] == '' else 'null', 'used': False}
+        DICT[key] = {'categories': cats, 'comments': row['comment'] if not row['comment'] == '' else 'null', 'used': False}
 with open('keywords.json', 'w') as o:
-    json.dump(dict, o)
+    json.dump(DICT, o)
