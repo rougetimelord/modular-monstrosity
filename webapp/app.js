04dea872683f83d81ee76c18d701a9999555bdff
Rouge <code@r0uge.org>
2018-02-07 16:30:25 -0800

    Fix js and add web stuff

--- webapp/app.js
+++ webapp/app.js
@@ -1,42 +1,48 @@
 var _storage = window.localStorage, _json;
 
 var main = function(){
-    console.log("JSON gotten");
+    
 }
 
 var json_downloader = function(){
-    let url = "/data/keywords.json", xhr = new XMLHttpRequest(), json_loader = () => {
-        data = this.responseText;
+    let url = "../data/keywords.json", xhr = new XMLHttpRequest(), json_loader = () => {
+        data = xhr.response;
         _json = JSON.parse(data);
         _storage.setItem("data", data);
+        console.log("JSON gotten");
         main();
     };
-    xhr.addEventListener("load", json_loader);
+    xhr.addEventListener("loadend", json_loader);
     xhr.open("GET", url);
     xhr.send();
 }
 
 var hash_check = function(){
-    let url = "/data/hash.txt", xhr = new XMLHttpRequest(), hash_handler = () => {
-        hash = this.responseText;
-        if(!_storage.getItem("data")){
+    let url = "../data/hash.txt", xhr = new XMLHttpRequest(), hash_handler = () => {
+        hash = xhr.response;
+        console.log(hash);
+        if(!_storage["data"]){
             json_downloader();
+            return
         }
         _json = JSON.parse(_storage.getItem("data"));
-        if (_json['hash'] != hash){
+        if (_json["hash"] != hash){
+            console.log("Cache miss")
             json_downloader();
         }
         else{
+            console.log("Cache hit");
             main();
+            return
         }
     };
-    xhr.addEventListener("load", hash_handler);
+    xhr.addEventListener("loadend", hash_handler);
     xhr.open("GET", url);
     xhr.send();
 }
 
 var bootloader = () => {
-    hash_check()
+    hash_check();
 }
 
-document.addEventListener("DOMContentLoaded", bootloader);
+document.addEventListener("load", bootloader);
