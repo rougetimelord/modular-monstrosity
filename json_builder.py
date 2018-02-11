57f6fc78b4141ac3e6beaead55d8ffc600eed887
Rouge <code@r0uge.org>
2018-02-11 14:38:40 -0800

    Fix hashing

--- json_builder.py
+++ json_builder.py
@@ -7,10 +7,7 @@ URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeYoWsTDrgykb-_xR6Z0U3ID
 REQ = urllib.request.Request(URL)
 RES = urllib.request.urlopen(REQ)
 RES_DATA = RES.read().decode('utf-8').replace('\r','')
-HASH = hashlib.sha256(RES_DATA.encode('utf-8')).hexdigest()[:7]
-DICT = {'hash': HASH, 'cats': []}
-with open('data/hash.txt', 'w') as h:
-    h.write(HASH)
+DICT = {'cats': []}
 with open('data/keyword.csv', 'w') as f:
     f.write(RES_DATA)
 with open('data/keyword.csv', 'r') as g:
@@ -29,5 +26,8 @@ with open('data/keyword.csv', 'r') as g:
         if len(cats) == 0:
             cats = 'null'
         DICT[key] = {'comments': row['comment'] if not row['comment'] == '' else 'null', 'used': False}
+HASH = hashlib.sha256(RES_DATA.encode('utf-8')).hexdigest()[:7]
+with open('data/hash.txt', 'w') as h:
+    h.write(HASH)
 with open('data/keywords.json', 'w') as o:
     json.dump(DICT, o)
