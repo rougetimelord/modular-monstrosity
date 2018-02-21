7008c3bb97d1755abdeb3dce20b0402bed3d6f3f
Rouge <code@r0uge.org>
2018-02-21 15:22:00 -0800

    Move build tools to their own folder

--- build_tools/json_builder.py
+++ build_tools/json_builder.py
@@ -8,9 +8,9 @@ REQ = urllib.request.Request(URL)
 RES = urllib.request.urlopen(REQ)
 RES_DATA = RES.read().decode('utf-8').replace('\r','')
 DICT = {'cats': []}
-with open('data/keyword.csv', 'w') as f:
+with open('../data/keyword.csv', 'w') as f:
     f.write(RES_DATA)
-with open('data/keyword.csv', 'r') as g:
+with open('../data/keyword.csv', 'r') as g:
     reader = csv.DictReader(g)
     for row in reader:
         key = row['keyword']
@@ -27,7 +27,7 @@ with open('data/keyword.csv', 'r') as g:
             cats = 'null'
         DICT[key] = {'comments': row['comment'] if not row['comment'] == '' else 'null', 'used': False}
 HASH = hashlib.sha256(RES_DATA.encode('utf-8')).hexdigest()[:7]
-with open('data/hash.txt', 'w') as h:
+with open('../data/hash.txt', 'w') as h:
     h.write(HASH)
-with open('data/keywords.json', 'w') as o:
+with open('../data/keywords.json', 'w') as o:
     json.dump(DICT, o)
